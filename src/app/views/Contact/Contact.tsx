b8d81db3906a3c72fc064a7fba26114da21ae2ae
berthoogsteyns <berthoogsteyns@gmail.com>
2020-10-22 10:28:41 +0200

    ft-contact-responsive

--- src/app/views/Contact/Contact.tsx
+++ src/app/views/Contact/Contact.tsx
@@ -4,12 +4,17 @@ import { GrTwitter } from 'react-icons/gr'
 import { GrSnapchat } from 'react-icons/gr'
 import { FaLinkedinIn } from 'react-icons/fa'
 import './Contact.scss'
+import { useMediaQuery } from 'react-responsive'
 
 type Props = {}
 
 export const Contact = React.memo((props: Props) => {
+
+  const isMobile = useMediaQuery({ maxDeviceWidth: 500 })
+
   return (
     <div className='c-container'>
+      
       <div className='c-container-left'>
         <h1 className='c-container-left-header'>Contact us</h1>
         <div className='c-container-left-info'>
@@ -35,23 +40,25 @@ export const Contact = React.memo((props: Props) => {
 
       <div className='c-container-divider'></div>
 
-      <div className='c-container-rigth'>
-        <form className='c-container-rigth-form'>
+      {isMobile ? <hr className='c-container-horizontal'/> : ''}
+
+      <div className='c-container-right'>
+        <form className='c-container-right-form'>
           <input
-            className='c-container-rigth-form-name'
+            className='c-container-right-form-name'
             type='text'
             placeholder='Name'
           />
           <input
-            className='c-container-rigth-form-email'
+            className='c-container-right-form-email'
             type='text'
             placeholder='Email'
           />
           <textarea
-            className='c-container-rigth-form-message'
+            className='c-container-right-form-message'
             placeholder='Message'
           />
-          <button className='c-container-rigth-form-button' type='submit'>
+          <button className='c-container-right-form-button' type='submit'>
             Submit
           </button>
         </form>
